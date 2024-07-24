fc9ba28c75cf1a4fe12cdac57fa545dd55cc52d9
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-07-24 11:33:13 +0545

    DbConnection

---
+++ DbConnection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "databaseconnection";
+
+//create connection 
+$connection = new mysqli($servername, $username, $password, $database);
+
+if($connection->connect_error) {
+    die("connection failed:" . $connection->connect_error);
+}
+echo"The database is connected successfully";
+?>
