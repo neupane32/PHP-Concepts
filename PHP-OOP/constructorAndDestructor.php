75b77fffb091b24fffb01eb26ecba29b71b7ece4
Aashish Neupane <np05cp4a220004@iic.edu.np>
2024-07-30 22:05:45 +0545

    constructor and destructor

--- PHP-OOP/constructorAndDestructor.php
+++ PHP-OOP/constructorAndDestructor.php
@@ -4,7 +4,7 @@ class Fruit
 {
     private $name;
     private $desc;
-    
+
     //constructor
     public function __construct($name, $desc)
     {
@@ -25,3 +25,26 @@ $apple = new Fruit("Apple", "An apple a day keep a doctor away!!");
 echo $apple->getname();
 echo "<br>";
 echo $apple->getdesc();
+echo "<br>";
+
+?>
+
+<?php
+
+//destructor
+class Fruits {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+$apple = new Fruits("Apple", "red");
+?>
+
